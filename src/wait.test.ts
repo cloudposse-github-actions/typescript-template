fb4644f853ff36dfc958fdd917d68b3589e381a4
Matt Calhoun <matt@cloudposse.com>
2022-10-03 12:07:29 -0400

    Feature/initial implementation (#3)

--- src/wait.test.ts
+++ src/wait.test.ts
@@ -1,6 +1,4 @@
 import { wait } from "./wait";
-import * as cp from "child_process";
-import * as path from "path";
 
 test("throws invalid number", async () => {
   const input = parseInt("foo", 10);
@@ -13,15 +11,4 @@ test("wait 500 ms", async () => {
   const end = new Date();
   const delta = Math.abs(end.getTime() - start.getTime());
   expect(delta).toBeGreaterThan(450);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test("test runs", () => {
-  process.env["INPUT_MILLISECONDS"] = "500";
-  const np = process.execPath;
-  const ip = path.join(__dirname, "..", "dist", "index.js");
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env,
-  };
-  console.log(cp.execFileSync(np, [ip], options).toString());
 });
